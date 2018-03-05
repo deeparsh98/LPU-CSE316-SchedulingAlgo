5c8e91f4e3b6ab49df44d6bf8e074e4ce94682e3
deeparsh98 <deep.bhargav98@gmail.com>
2018-03-05 18:50:43 +0530

    Added Process Register and Simulation modules
    -Process register module basically registers a new process in the memory just like storing the PCB in the memory.
    -Simulation module is the start point of the entire Algorithm.

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdbool.h>
-
-int ID_COUNTER=0;
-
+#include<stdlib.h>
+int ID_COUNTER=0,TIMER=0;
+const char ST_ARRAY[][11]={"TERMINATED","WAITING","RUNNING","READY"};
 //STRUCTURES AND SKELETONS
 enum States{
 TERMINATED,WAITING,RUNNING,READY
@@ -36,9 +36,54 @@ struct ProcessStructure ProcessInitialiser(int PID,int ERT){
 	//priority
 	PriorityAssigner(&p);
 	p.state=READY;
-	p.ArrivalTime=CPU.TIMER;
+	p.ArrivalTime=TIMER;
 	return p;
 }
-int main(){}
 
+struct ProcessStructure* ProcessRegister(){
+	int ch;
+	int temp_ERT;
+	struct ProcessStructure* P=(struct ProcessStructure*)malloc(sizeof(struct ProcessStructure));
+	int i=0; //iterator for initialisation of structures;
+	do{
+		printf("ENTER ERT\n");
+		scanf("%d",&temp_ERT);
+		*(P+i)=ProcessInitialiser(++ID_COUNTER,temp_ERT);
+		//ALLOCATING MEMORY FOR ANOTHER PROCESS
+		printf("ANYMORE PROCESS?\n");
+		scanf("%d",&ch);
+		if(ch){
+			P=realloc(P,(i+2)*sizeof(struct ProcessStructure));
+			i++;
+		}
+	}while(ch);
+	//PRINTING FOR DEBUGING...
+	while(i>=0){
+		printf("PID : %d\nERT : %d\nWaittime : %d\nPriority : %d\nAT : %d\nState : %s\n",(P+i)->PID,(P+i)->ERT,(P+i)->WaitTime,(P+i)->priority,(P+i)->ArrivalTime,ST_ARRAY[(P+i)->state]);
+		i--;
+	}
+	return P;
 
+}
+bool Simulation(int timer,int pidCount){
+	TIMER=timer;
+	ID_COUNTER=pidCount;
+	struct ProcessStructure* processes=ProcessRegister();
+	pid_t pid=fork();
+	switch(pid){
+	case 0:
+		while(1){
+			sleep(1);
+			TIMER++;
+		}
+		break;
+	default:
+	}
+
+	free(processes);
+
+}
+
+int main(){
+	Simulation(0,0);
+}
